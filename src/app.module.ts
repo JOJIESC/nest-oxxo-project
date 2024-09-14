74a6eb013607714a77bb7484788e1fb9fff15c8e
jorgescx <j_escobart@hotmail.com>
2024-09-14 09:49:28 -0600

    fix enviroment password varible integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,20 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EmployeesModule } from "./employees/employees.module";
 import { ProductsModule } from "./products/products.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "postgres",
       host: process.env.host,
       port: +process.env.port,
       username: "postgres",
-      password: "password",
+      password: process.env.pass,
       database: process.env.name,
       entities: [],
+      autoLoadEntities: true,
       synchronize: true,
     }),
     EmployeesModule,
