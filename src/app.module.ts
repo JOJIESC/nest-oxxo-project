ddf0e719e71e853c288c1566410995c6daca9527
jorgescx <j_escobart@hotmail.com>
2024-09-13 22:57:55 -0600

    DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EmployeesModule } from './employees/employees.module';
-import { ProductsModule } from './products/products.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { EmployeesModule } from "./employees/employees.module";
+import { ProductsModule } from "./products/products.module";
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.host,
+      port: +process.env.port,
+      username: "postgres",
+      password: "password",
+      database: process.env.name,
+      entities: [],
+      synchronize: true,
+    }),
+    EmployeesModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
