320c40cf52f7d3b7e39bcbae0fce289c257582df
jorgescx <j_escobart@hotmail.com>
2024-10-03 17:13:44 -0600

    36-AuthGuard

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -3,9 +3,20 @@ import { ProvidersService } from "./providers.service";
 import { ProvidersController } from "./providers.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Provider } from "./entities/provider.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { JWT_KEY, EXPIRES_IN } from "../auth/constants/jwt.constants";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Provider])],
+  imports: [
+    JwtModule.register({
+      secret: JWT_KEY,
+      signOptions: {
+        expiresIn: EXPIRES_IN,
+      },
+      global: true,
+    }),
+    TypeOrmModule.forFeature([Provider]),
+  ],
   controllers: [ProvidersController],
   providers: [ProvidersService],
 })
