0c67f8b906c79fa241d34d8b01460b69044737ad
jorgescx <j_escobart@hotmail.com>
2024-10-03 18:07:11 -0600

    39 Roles Decorator

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -15,7 +15,9 @@ import { UpdateProviderDto } from "./dto/update-provider.dto";
 import { NotFoundException } from "@nestjs/common";
 import { AuthGuard } from "src/auth/guards/auth.guard";
 import { User } from "src/auth/entities/user.entity";
-import { UserData } from "src/auth/decorators/user.decorators";
+import { UserData } from "src/auth/decorators/user.decorator";
+import { Roles } from "src/auth/decorators/roles.decorator";
+import { RolesGuard } from "src/auth/guards/roles.guard";
 
 @UseGuards(AuthGuard)
 @Controller("providers")
@@ -27,6 +29,8 @@ export class ProvidersController {
     return this.providersService.create(createProviderDto);
   }
 
+  @Roles(["Admin"])
+  @UseGuards(RolesGuard)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes("Employee"))
