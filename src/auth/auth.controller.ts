69ebf64be3427aef1661d763cca459c9bfe22835
jorgescx <j_escobart@hotmail.com>
2024-10-19 19:50:04 -0600

    add: cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Param, Patch, Post, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { LoginUserDto } from "./dto/login-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { ApiAuth } from "src/auth/decorators/api.decorator";
 import { ApiTags } from "@nestjs/swagger";
+import { Response } from "express";
+import { TOKEN_NAME } from "./constants/jwt.constants";
+import { Cookies } from "./decorators/cookies.decorator";
 @ApiAuth()
 @ApiTags("AUTH")
 @Controller("auth")
@@ -17,8 +20,18 @@ export class AuthController {
   }
 
   @Post("login")
-  login(@Body() loginUserDto: LoginUserDto) {
-    return this.authService.loginUser(loginUserDto);
+  login(
+    @Body() loginUserDto: LoginUserDto,
+    @Res({ passthrough: true }) response: Response,
+    @Cookies() cookies: any
+  ) {
+    const token = this.authService.loginUser(loginUserDto);
+    response.cookie(TOKEN_NAME, token, {
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
   }
 
   @Patch("/:email")
